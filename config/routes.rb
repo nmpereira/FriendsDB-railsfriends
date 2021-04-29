e59f032b66a0b87cc426182ea5e9f41c19a035c8
nmpereira <nikhilpereira25@gmail.com>
2021-04-29 18:46:05 -0400

    Update page layout, routes and CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :friends
-  get 'home/about'
-  root 'home#index'
+  #get 'home/about'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
